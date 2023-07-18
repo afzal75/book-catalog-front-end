485820c5eb3a81953265d2350db57cb99fd65a04
afzal75 <afzalbhuiyan16@gmail.com>
2023-07-19 01:31:07 +0600

    store created successfully

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@material-tailwind/react'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routes/routes.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </Provider>
+    <ToastContainer />
+  </React.StrictMode>
 )
